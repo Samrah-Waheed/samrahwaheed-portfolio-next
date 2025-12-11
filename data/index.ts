c2eb1e31cf771f4e190877702fb581de969a79c5
Samrah Waheed <samrahwaheed484@gmail.com>
2025-12-11 05:32:19 -0800

    Initial commit

---
+++ data/index.ts
@@ -0,0 +1,217 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Building seamless web experiences through open collaboration",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Available across time zone for smooth collaboration",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "Always exploring learning, and improving my web development skills.",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Frontend developer passionate about crafting elegant digital experiences.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "3D Solar System (Learning Project)",
+    des: "Practiced creating immersive 3D visuals using Three.js and React. Explored planet motion, lighting, and camera controls to understand interactive 3D web experiences.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Yoom Video App (Practice Project)",
+    des: "Customized a video conferencing UI while learning Next.js and TypeScript. Focused on layout design, responsive components, and smooth user interaction.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "AI Canvas (Learning Project)",
+    des: "Explored AI-powered image generation concepts. Practiced front-end integration and dynamic design rendering for modern creative web applications.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "3D iPhone Product Page (Practice Project)",
+    des: "Learned 3D product visualization and interactive motion effects with React Three Fiber. Focused on performance and realistic lighting animations.",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Samrah was an absolute pleasure. Her professionalism, creativity, and dedication to writing clean and modern frontend code were evident throughout our project. Samrah’s passion for UI/UX and attention to detail truly stand out. If you're seeking to elevate your website and improve your brand experience, Samrah is the ideal developer to work with.",
+    name: "",
+    title: "Founder of NovaTech Solutions",
+  },
+  {
+    quote:
+      "Collaborating with Samrah was an absolute pleasure. Her professionalism, creativity, and dedication to writing clean and modern frontend code were evident throughout our project. Samrah’s passion for UI/UX and attention to detail truly stand out. If you're seeking to elevate your website and improve your brand experience, Samrah is the ideal developer to work with.",
+    name: "Michael Jahnson",
+    title: "Founder of NovaTech Solutions",
+  },
+  {
+    quote:
+      "Collaborating with Samrah was an absolute pleasure. Her professionalism, creativity, and dedication to writing clean and modern frontend code were evident throughout our project. Samrah’s passion for UI/UX and attention to detail truly stand out. If you're seeking to elevate your website and improve your brand experience, Samrah is the ideal developer to work with.",
+    name: "Michael Jahnson",
+    title: "Founder of NovaTech Solutions",
+  },
+  {
+    quote:
+      "Collaborating with Samrah was an absolute pleasure. Her professionalism, creativity, and dedication to writing clean and modern frontend code were evident throughout our project. Samrah’s passion for UI/UX and attention to detail truly stand out. If you're seeking to elevate your website and improve your brand experience, Samrah is the ideal developer to work with.",
+    name: "Michael Jahnson",
+    title: "Founder of NovaTech Solutions",
+  },
+  {
+    quote:
+      "Collaborating with Samrah was an absolute pleasure. Her professionalism, creativity, and dedication to writing clean and modern frontend code were evident throughout our project. Samrah’s passion for UI/UX and attention to detail truly stand out. If you're seeking to elevate your website and improve your brand experience, Samrah is the ideal developer to work with.",
+    name: "Michael Jahnson",
+    title: "Founder of NovaTech Solutions",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in building responsive web interfaces using React.js, improving UI speed and enhancing overall user experience.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Frontend Developer - JSM Tech",
+    desc: "Developed modern, interactive web pages with React.js and JavaScript, ensuring clean UI design and smooth performance.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance Frontend Developer",
+    desc: "Designed and delivered complete frontend solutions for clients, focusing on UI layouts, animations, and responsive designs.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained advanced user-facing features using modern frontend technologies, ensuring high-quality interfaces.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
